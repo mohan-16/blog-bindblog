573e8bc79ddeadf32cc6b3878a5c36c863d50514
mohan-16 <mohanrajg1630@gmail.com>
2024-03-02 12:40:22 +0530

    update button and first header section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import styles from "./featured.module.css";
 import Image from "next/image";
+import Button from "../button/Button";
 
 const Featured = () => {
   return (
     <div className={styles.container}>
-    <h1 className={styles.title}>
-      <b>Hey, lama dev here!</b> Discover my stories and creative ideas.
-    </h1>
-    <div className={styles.post}>
-      <div className={styles.imgContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
-      <div className={styles.textContainer}>
-        <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet alim consectetur adipisicing elit.</h1>
-        <p className={styles.postDesc}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Cupiditate, quam nisi magni ea laborum inventore voluptatum
-          laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
-          quisquam! Harum unde sit culpa debitis.
-        </p>
-        <button className={styles.button}>Read More</button>
+      <h1 className={styles.title}>
+        <b>Hey, Mohan here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+
+          <Image src="/p1.jpeg" alt="" width={550} height={450} className={styles.image}  />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            {" "}
+            "Coastal Peaks: A Quest Along the Sea and Mountains"
+          </h1>
+          <p className={styles.postDesc}>
+            Embark on an unforgettable adventure along the rugged coastlines and
+            towering peaks where the sea meets the mountains. Discover hidden
+            coves, majestic cliffs, and breathtaking vistas as you navigate the
+            untamed beauty of this enchanting landscape. Join us as we journey
+            to the edge of the world and explore the harmony of nature's most
+            captivating realms.
+          </p>
+          {/* <button className={styles.button}>Read More</button> */}
+        <Button button="Read more"/>
+         </div>
       </div>
     </div>
-  </div>
   );
 };
 
